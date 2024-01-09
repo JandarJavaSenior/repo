e6bc9afd3c8958bae34cc842b106708b76a20ddb
Jandar <xp3335@gmail.com>
2024-01-09 17:22:35 +0600

    ccc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("jandar hello");
     }
+
+
+    List<Integer> list=new ArrayList<>();
+
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+
 }
